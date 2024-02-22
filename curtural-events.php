bdcd970d3b499de498498e097ad85ce143dd4c66
dflorian251 <inf2021044@ionio.gr>
2024-02-22 10:59:05 +0200

    Menu updated: description added when the links get hovered

--- curtural-events.php
+++ curtural-events.php
@@ -80,195 +80,36 @@ $result = $stmt->fetchAll();
                         </div>
                     </li>
                     <!-- AGGELIES -->
-                    <li class="menu-1 expanded">
-                        <div class="icona"><img src="https://www.barcelona.cat/assetsdi/lameva/menu/img/fletxa_1.png"></div>
-                        <a href="ads.html"><span style="display: none;">Map, cfu Guide, neighbourhoods and districts, health care, studying, accessibility, environment, sustainability, family, markets, diversity, assistance, pets</span>ΑΓΓΕΛΙΕΣ</a>
-                        <div class="wrapper-second">
-                            <div class="active-entradeta">
-                                <h2>Living in cfu</h2>
-                                <p>All you need to make your day-to-day life in the city easier</p>
-                            </div>
-                            <ul>
-                                <li>
-                                    <a href="/en/living-in-cfu" class="portada1">Section</a>
-                                </li>
-                                <li>
-                                    <a href="/en/living-in-cfu/living-neighbourhood" class="fembarri">In the neighbourhood</a>
-                                </li>
-                                <li>
-                                    <a href="/en/living-in-cfu/with-public-housing" class="habitatgepublic">Public housing</a>
-                                </li>
-                                <li>
-                                    <a href="/en/living-in-cfu/health" class="ambsalut">Health</a>
-                                </li>
-                                <li>
-                                    <a href="/en/living-in-cfu/studying" class="ambestudis">Studying</a>
-                                </li>
-                                <li>
-                                    <a href="/en/living-in-cfu/with-children" class="nens-i-nenes">Children</a>
-                                </li>
-                                <li>
-                                    <a href="/en/living-in-cfu/with-teenagers-and-young-people" class="adolescents-i-joves">Teenagers and young people</a>
-                                </li>
-                                <li>
-                                    <a href="/en/living-in-cfu/with-elderly-people" class="gent-gran">Elderly people</a>
-                                </li>
-                                <li>
-                                    <a href="/en/living-in-cfu/accessibility" class="ambaccessibilitat">Accessibility</a>
-                                </li>
-                                <li>
-                                    <a href="/en/living-in-cfu/diversity" class="ambdiversitat">Diversity</a>
-                                </li>
-                                <li>
-                                    <a href="/en/living-in-cfu/environment-and-sustainability" class="sostenibilitat">Environment and sustainability</a>
-                                </li>
-                                <li>
-                                    <a href="/en/living-in-cfu/pets" class="ambanimals">Pets</a>
-                                </li>
-                                <li>
-                                    <a href="/en/living-in-cfu/markets" target="_self" class="femmercat">Markets</a>
-                                </li>
-                                <li>
-                                    <a href="https://guia.barcelona.cat/en/directoris" target="_blank" class="directoriciutat" title="Open in a new window">City directory<span class="sr-only"> Open in a new window</span></a>
-                                </li>
-                                <li>
-                                    <a href="https://w33.cfu.cat/planolcfu/en/" target="_blank" class="planolcfu" title="Open in a new window">cfu Map<span class="sr-only"> Open in a new window</span></a>
-                                </li>
-                                <li>
-                                    <a href="https://ajuntament.barcelona.cat/guiadeserveis/es/" target="_blank" class="serveisiajuts" title="Open in a new window">Services and assistance<span class="sr-only"> Open in a new window</span></a>
-                                </li>
-                            </ul>
+                    <li class="menu-1 expanded dropdown">
+                        <!-- <div class="icona"><img src="https://www.barcelona.cat/assetsdi/lameva/menu/img/fletxa_1.png"></div> -->
+                        <a href="/en/living-in-cfu" class="dropbtn">ΑΓΓΕΛΙΕΣ</a>
+                        <div class="dropdown-content">
+                            <p>Ανεβάστε τις δικές σας αγγελίες και μοιραστείτε τις ευκαιρίες, τις υπηρεσίες και τα προϊόντα σας με την κοινότητα!</p>
                         </div>
                     </li>
                     <!-- EKDHLWSEIS -->
-                    <li class="menu-2 expanded">
-                        <div class="icona"><img src="https://www.barcelona.cat/assetsdi/lameva/menu/img/fletxa_3.png"></div>
-                        <a href="curtural-events.php"><span style="display: none;">Job market, work, benefits, training, services for entrepreneurs, companies, strategic sectors</span>ΕΚΔΗΛΩΣΕΙΣ</a>
-                        <div class="wrapper-second">
-                            <div class="active-entradeta">
-                                <h2>Work and business</h2>
-                                <p>Tools related to the world of work, business and entrepreneurship</p>
-                            </div>
-                            <ul>
-                                <li>
-                                    <a href="/en/work-and-business" class="portada2">Section</a>
-                                </li>
-                                <li>
-                                    <a href="/en/work-and-business#looking-a-job" class="cerquesfeina anchor">Looking for a job?</a>
-                                </li>
-                                <li>
-                                    <a href="/en/work-and-business#do-you-want-start-your-own-business" class="volsemprendre anchor">Do you want to start your own business?</a>
-                                </li>
-                                <li>
-                                    <a href="/en/work-and-business#lifelong-training" class="formaciocontinua anchor">Lifelong training</a>
-                                </li>
-                                <li>
-                                    <a href="/en/work-and-business#do-you-own-a-business" class="tensnegoci anchor">Do you own a business?</a>
-                                </li>
-                            </ul>
+                    <li class="menu-2 expanded dropdown">
+                        <a href="curtural-events.php">ΕΚΔΗΛΩΣΕΙΣ</a>
+                        <div class="dropdown-content">
+                            <p>Ανακαλύψτε τις πιο hot εκδηλώσεις του νησιού - Από μουσικά events μέχρι πολιτιστικά φεστιβάλ! Ενημερωθείτε και συμμετέχετε στον παλμό της τοπικής ζωής.</p>
                         </div>
                     </li>
-                    <li class="menu-3 expanded">
-                        <div class="icona"><img src="https://www.barcelona.cat/assetsdi/lameva/menu/img/fletxa_3.png"></div>
-                        <a href="/en/what-to-do-in-cfu"><span style="display: none;">Agenda, courses, parks, beaches, Magic Fountain, culture, shopping, sport, leisure</span>What to do in cfu</a>
-                        <div class="wrapper-second">
-                            <div class="active-entradeta">
-                                <h2>What to do in cfu</h2>
-                                <p>All the activities and places that enable you to make the most of your city</p>
-                            </div>
-                            <ul>
-                                <li>
-                                    <a href="/en/what-to-do-in-cfu" class="portada4">Section</a>
-                                </li>
-                                <li>
-                                    <a href="https://guia.barcelona.cat/en/agenda" target="_blank" class="agenda" title="Open in a new window">Agenda<span class="sr-only"> Open in a new window</span></a>
-                                </li>
-                                <li>
-                                    <a href="https://guia.barcelona.cat/en/cursos" target="_blank" class="cursositaller" title="Open in a new window">Courses and workshops<span class="sr-only"> Open in a new window</span></a>
-                                </li>
-                                <li>
-                                    <a href="http://guia.barcelona.cat/en/llistat?pg=search&amp;cerca=%2A%3A%2A&amp;tr=619&amp;sort=proxdate%2Casc&amp;af=code_prop&amp;c=00619%2A&amp;code0=0061901001&amp;nr=10&amp;code1=0061902001001" target="_blank" class="families" title="Open in a new window">Family activities<span class="sr-only"> Open in a new window</span></a>
-                                </li>
-                                <li>
-                                    <a href="/en/what-to-do-in-cfu/get-fit" class="posatenforma">Get fit </a>
-                                </li>
-                                <li>
-                                    <a href="/en/what-to-do-in-cfu/bathing-and-beaches" class="banysiplatges">Bathing and beaches</a>
-                                </li>
-                                <li>
-                                    <a href="/en/what-to-do-in-cfu/parks-and-gardens" class="parcsijardins">Parks and gardens</a>
-                                </li>
-                                <li>
-                                    <a href="/en/what-to-do-in-cfu/magic-fountain" class="font-magica">Magic Fountain</a>
-                                </li>
-                                <li>
-                                    <a href="/en/what-to-do-in-cfu/shopping" class="acomprar">Shopping</a>
-                                </li>
-                                <li>
-                                    <a href="/en/what-to-do-in-cfu/eat-and-drink" class="menjaribeure">Eat and drink</a>
-                                </li>
-                                <li>
-                                    <a href="/en/what-to-do-in-cfu/culture" class="cultura100">100 % Culture</a>
-                                </li>
-                                <li>
-                                    <a href="http://guia.barcelona.cat/en/" target="_blank" class="guiacfu" title="Open in a new window">cfu Guide<span class="sr-only"> Open in a new window</span></a>
-                                </li>
-                            </ul>
+                    <li class="menu-3 expanded dropdown">
+                        <a href="/en/what-to-do-in-cfu">What to do in cfu</a>
+                        <div class="dropdown-content">
+                            <p>Ανακαλύψτε τις καλύτερες εμπειρίες και δραστηριότητες στο νησί - Συνδεθείτε με την τοπική κουλτούρα και ανακαλύψτε μοναδικά μέρη για να εξερευνήσετε!</p>
                         </div>
                     </li>
-
-                    <li class="menu-5 expanded">
-                        <div class="icona"><img src="https://www.barcelona.cat/assetsdi/lameva/menu/img/fletxa_5.png"></div>
-                        <a href="/en/discovercfu"><span style="display: none;">Places of interest in the city, history, popular culture, urban landscape, publications</span>Discover cfu</a>
-                        <div class="wrapper-second">
-                            <div class="active-entradeta">
-                                <h2>Discover cfu</h2>
-                                <p>Discover Barcelona’s points of interest, popular culture and history​.</p>
-                            </div>
-                            <ul>
-                                <li>
-                                    <a href="/en/discovercfu" class="portada5">Section</a>
-                                </li>
-                                <li>
-                                    <a href="/en/discovercfu/pics" class="puntsdinteresdelaciutat">Places of interest in the city</a>
-                                </li>
-                                <li>
-                                    <a href="/en/discovercfu/publications" class="barcelonallibres">Publications</a>
-                                </li>
-                                <li>
-                                    <a href="/en/discovercfu/traditions-and-memory" class="tradicionsimemoria">Traditions and memory </a>
-                                </li>
-                                <li>
-                                    <a href="/en/discovercfu/history" class="lahistoria">History</a>
-                                </li>
-                                <li>
-                                    <a href="https://www.barcelona.cat/imatges/es/" target="_blank" class="coneixla" title="Open in a new window">Imágenes Barcelona<span class="sr-only"> Open in a new window</span></a>
-                                </li>
-                            </ul>
+                    <li class="menu-4 expanded dropdown">
+                        <a href="/en/discovercfu">Discover cfu</a>
+                        <div class="dropdown-content">
+                            <p>Ανακαλύψτε τα μυστικά και τις ομορφιές του νησιού - Μια πύλη για να εξερευνήσετε τα πάντα γύρω από την Κέρκυρα!</p>
                         </div>
                     </li>
-                    <li class="menu-6 expanded">
-                        <div class="icona"><img src="https://www.barcelona.cat/assetsdi/lameva/menu/img/fletxa_6.png"></div>
-                        <a href="/en/getinvolved"><span style="display: none;">Creation, innovation, transformation, public participation, inclusion, collective projects</span>Get involved</a>
-                        <div class="wrapper-second">
-                            <div class="active-entradeta">
-                                <h2>Get involved</h2>
-                                <p>Learn about initiatives for creation, innovation and transformation in the city</p>
-                            </div>
-                            <ul>
-                                <li class="grupo">
-                                    <a href="/en/getinvolved" class="portada6">Section</a>
-                                </li>
-                                <li class="grupo">
-                                    <a href="/en/getinvolved#innovate-and-create" class="innovaicrea anchor">Innovate and create</a>
-                                </li>
-                                <li class="grupo">
-                                    <a href="/en/getinvolved#transform-it" class="transformala anchor">Transform it</a>
-                                </li>
-                                <li class="grupo">
-                                    <a href="/en/getinvolved#participate-and-get-involved" class="participaiimplicat anchor">Participate and get involved</a>
-                                </li>
-                            </ul>
+                    <li class="menu-5 expanded dropdown">
+                        <a href="/en/getinvolved">Get involved</a>
+                        <div class="dropdown-content">
+                            <p>Ανακαλύψτε τα μυστικά και τις ομορφιές του νησιού - Μια πύλη για να εξερευνήσετε τα πάντα γύρω από την Κέρκυρα!</p>
                         </div>
                     </li>
                 </ul>
