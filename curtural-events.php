11cdbc3f12e614652cf413d197fa096f62c30c33
dflorian251 <inf2021044@ionio.gr>
2024-02-22 12:33:43 +0200

    Micro changes on navigation

--- curtural-events.php
+++ curtural-events.php
@@ -82,7 +82,7 @@ $result = $stmt->fetchAll();
                     <!-- AGGELIES -->
                     <li class="menu-1 expanded dropdown">
                         <!-- <div class="icona"><img src="https://www.barcelona.cat/assetsdi/lameva/menu/img/fletxa_1.png"></div> -->
-                        <a href="/en/living-in-cfu" class="dropbtn">ΑΓΓΕΛΙΕΣ</a>
+                        <a href="ads.html" class="dropbtn">ΑΓΓΕΛΙΕΣ</a>
                         <div class="dropdown-content">
                             <p>Ανεβάστε τις δικές σας αγγελίες και μοιραστείτε τις ευκαιρίες, τις υπηρεσίες και τα προϊόντα σας με την κοινότητα!</p>
                         </div>
--- post.php
+++ post.php
@@ -17,12 +17,86 @@ if (isset($_GET['id'])) {
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="css/post_style.css">   
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <head>  
+        <link rel="stylesheet" href="barcelona/menu.css">
+
+
+        <!-- <link rel="stylesheet" media="all" href="barcelona/core.css"> -->
+        <!-- <link rel="stylesheet" media="all" href="barcelona/css_OIbdenO1m2T6Gl8g0oFZmK7R3OTEWlUSxrnlyc4gTLM.css"> -->
+
+        
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/header.css">
+        
+
+        <link rel="stylesheet" media="all" href="barcelona/css_rFRysy6MbVSEpojtuKXhCIczox98m6-B1zLbaxABV_E.css">
+        <link rel="stylesheet" media="all" href="barcelona/css_F4e7z303Ny2ifV3z3tSupMhjmCvWXEmfg1ViqTzA3ZA.css">
+        <link rel="stylesheet" href="css/post_style.css"> 
+
         <script src="https://kit.fontawesome.com/c4f6644410.js" crossorigin="anonymous"></script>
     </head>
     <body>
+    <header class="header">
+        <ul type="none">
+            <li class="logo"><a href="main.html">www.corfu.gr</a></li>
+            <div>
+                <i class="fa-solid fa-earth-europe"></i>
+                <select id="languageSelector">
+                    <option value="en">Ελληνικά</option>
+                    <option value="fr">English</option>
+                    <option value="es">Spanish</option>
+                    
+                </select>
+            </div>
+            
+            <li class="municipality_container"><img src="" id="municipality_logo"></li>
+        </ul>
+    </header>
+
+    <div id="menu-ciutat-cfu-v1" class="desktop amagat" style="margin-bottom: 4rem;">
+        <div id="menu-ciutat-wrapper">
+            <ul data-region="menu" class="clearfix level-0">
+                <li class="menu-0">
+                    <a href="/en#hola-en"><img width="22" height="22" src="https://www.barcelona.cat/assetsdi/lameva/menu/img/ico00.png"></a>
+                    <div>
+                    </div>
+                </li>
+                <!-- AGGELIES -->
+                <li class="menu-1 expanded dropdown">
+                    <!-- <div class="icona"><img src="https://www.barcelona.cat/assetsdi/lameva/menu/img/fletxa_1.png"></div> -->
+                    <a href="ads.html" class="dropbtn">ΑΓΓΕΛΙΕΣ</a>
+                    <div class="dropdown-content">
+                        <p>Ανεβάστε τις δικές σας αγγελίες και μοιραστείτε τις ευκαιρίες, τις υπηρεσίες και τα προϊόντα σας με την κοινότητα!</p>
+                    </div>
+                </li>
+                <!-- EKDHLWSEIS -->
+                <li class="menu-2 expanded dropdown">
+                    <a href="curtural-events.php">ΕΚΔΗΛΩΣΕΙΣ</a>
+                    <div class="dropdown-content">
+                        <p>Ανακαλύψτε τις πιο hot εκδηλώσεις του νησιού - Από μουσικά events μέχρι πολιτιστικά φεστιβάλ! Ενημερωθείτε και συμμετέχετε στον παλμό της τοπικής ζωής.</p>
+                    </div>
+                </li>
+                <li class="menu-3 expanded dropdown">
+                    <a href="/en/what-to-do-in-cfu">What to do in cfu</a>
+                    <div class="dropdown-content">
+                        <p>Ανακαλύψτε τις καλύτερες εμπειρίες και δραστηριότητες στο νησί - Συνδεθείτε με την τοπική κουλτούρα και ανακαλύψτε μοναδικά μέρη για να εξερευνήσετε!</p>
+                    </div>
+                </li>
+                <li class="menu-4 expanded dropdown">
+                    <a href="/en/discovercfu">Discover cfu</a>
+                    <div class="dropdown-content">
+                        <p>Ανακαλύψτε τα μυστικά και τις ομορφιές του νησιού - Μια πύλη για να εξερευνήσετε τα πάντα γύρω από την Κέρκυρα!</p>
+                    </div>
+                </li>
+                <li class="menu-5 expanded dropdown">
+                    <a href="/en/getinvolved">Get involved</a>
+                    <div class="dropdown-content">
+                        <p>Ανακαλύψτε τα μυστικά και τις ομορφιές του νησιού - Μια πύλη για να εξερευνήσετε τα πάντα γύρω από την Κέρκυρα!</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
     <?php foreach ($result as $row){ ?>
         <article>
             <div class="post-header">
