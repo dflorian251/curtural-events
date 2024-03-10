330978633cc94e18a55db0a29c8193398abacdc4
dflorian251 <inf2021044@ionio.gr>
2024-03-10 10:29:43 +0200

    Responsive menu: added but still is shit

--- adverts.php
+++ adverts.php
@@ -21,6 +21,8 @@ $result = $stmt->fetchAll();
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="css/header.css">
         <link rel="stylesheet" href="barcelona/menu.css">
+        <link rel="stylesheet" href="css/responsive_header.css">
+        <link rel="stylesheet" href="css/responsive_menu.css">
 
         <link rel="stylesheet" href="css/adverts.css">
 
@@ -39,29 +41,73 @@ $result = $stmt->fetchAll();
     
     <body>
         <header class="header">
-            <ul type="none">
-                <li class="logo"><a href="main.html">www.corfu.gr</a></li>
-                <div>
-                    <i class="fa-solid fa-earth-europe"></i>
-                    <select id="languageSelector">
-                        <option value="en">Ελληνικά</option>
-                        <option value="fr">English</option>
-                        <option value="es">Spanish</option>
-                        
-                    </select>
-                </div>
-                
-                <li class="municipality_container"><img src="" id="municipality_logo"></li>
-            </ul>
-        </header>
+                <ul type="none">
+                    <input type="checkbox" id="menu-check">
+                    <li class="logo"><a href="main.html">www.corfu.gr</a></li>
+                    <li class="language_container">
+                        <div>
+                            <i class="fa-solid fa-earth-europe"></i>
+                            <select id="languageSelector">
+                                <option value="en">Ελληνικά</option>
+                                <option value="fr">English</option>
+                                <option value="es">Spanish</option>
+                            </select>
+                        </div>
+                    </li>
+                    
+                    <li class="dummy-list-item"> </li>
+                    <li class="menu-btn">
+                        <label for="menu-check" id="menu-btn">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </label>
+                    </li>
 
-        <div id="menu-ciutat-cfu-v1" class="desktop amagat" style="margin-bottom: 4rem;">
-            <div id="menu-ciutat-wrapper">
-                <ul data-region="menu" class="clearfix level-0">
+                    <div class="header-menu-container" id="header-menu-container">
+                        <ul id="header-menu-container-list">
+                            <!-- AGGELIES -->
+                            <li class="menu-1 expanded dropdown">
+                                <a href="adverts.php" class="dropbtn">ΑΓΓΕΛΙΕΣ</a>
+                                <div class="dropdown-content">
+                                    <p>Ανεβάστε τις δικές σας αγγελίες και μοιραστείτε τις ευκαιρίες, τις υπηρεσίες και τα προϊόντα σας με την κοινότητα!</p>
+                                </div>
+                            </li>
+                            <!-- EKDHLWSEIS -->
+                            <li class="menu-2 expanded dropdown">
+                                <a href="curtural-events.php">ΕΚΔΗΛΩΣΕΙΣ</a>
+                                <div class="dropdown-content">
+                                    <p>Ανακαλύψτε τις πιο hot εκδηλώσεις του νησιού - Από μουσικά events μέχρι πολιτιστικά φεστιβάλ! Ενημερωθείτε και συμμετέχετε στον παλμό της τοπικής ζωής.</p>
+                                </div>
+                            </li>
+                            <li class="menu-3 expanded dropdown">
+                                <a href="/en/what-to-do-in-cfu">What to do in cfu</a>
+                                <div class="dropdown-content">
+                                    <p>Ανακαλύψτε τις καλύτερες εμπειρίες και δραστηριότητες στο νησί - Συνδεθείτε με την τοπική κουλτούρα και ανακαλύψτε μοναδικά μέρη για να εξερευνήσετε!</p>
+                                </div>
+                            </li>
+                            <li class="menu-4 expanded dropdown">
+                                <a href="/en/discovercfu">Discover cfu</a>
+                                <div class="dropdown-content">
+                                    <p>Ανακαλύψτε τα μυστικά και τις ομορφιές του νησιού - Μια πύλη για να εξερευνήσετε τα πάντα γύρω από την Κέρκυρα!</p>
+                                </div>
+                            </li>
+                            <li class="menu-5 expanded dropdown">
+                                <a href="/en/getinvolved">Get involved</a>
+                                <div class="dropdown-content">
+                                    <p>Ανακαλύψτε τα μυστικά και τις ομορφιές του νησιού - Μια πύλη για να εξερευνήσετε τα πάντα γύρω από την Κέρκυρα!</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </ul>
+            </header>
+
+            <div class="menu-container" id="largeMenu" style="margin-bottom: 4rem;">
+                <ul>
                     <!-- AGGELIES -->
                     <li class="menu-1 expanded dropdown">
-                        <!-- <div class="icona"><img src="https://www.barcelona.cat/assetsdi/lameva/menu/img/fletxa_1.png"></div> -->
-                        <a href="#" class="dropbtn">ΑΓΓΕΛΙΕΣ</a>
+                        <a href="adverts.php" class="dropbtn">ΑΓΓΕΛΙΕΣ</a>
                         <div class="dropdown-content">
                             <p>Ανεβάστε τις δικές σας αγγελίες και μοιραστείτε τις ευκαιρίες, τις υπηρεσίες και τα προϊόντα σας με την κοινότητα!</p>
                         </div>
@@ -93,7 +139,6 @@ $result = $stmt->fetchAll();
                     </li>
                 </ul>
             </div>
-        </div>
 
         <div id="menu-ciutat-cfu-v1" class="desktop amagat adverts-options-container" style="margin-bottom: 4rem;">
             <div id="menu-ciutat-wrapper">
@@ -156,5 +201,40 @@ $result = $stmt->fetchAll();
         </div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script>
+            const menuBtn = document.getElementById("menu-btn");
+            const responMenu = document.getElementById("header-menu-container");
+            const responMenuList = document.getElementById("header-menu-container-list");
+            var isClicked = false;
+
+            
+            menuBtn.addEventListener("click", function () {
+                if (!isClicked) {
+                    //Responsive Menu
+                    responMenu.style.backgroundColor = "whitesmoke";
+                    responMenu.style.display = "flex";
+                    responMenu.style.transition = "opacity 250ms ease-in-out";
+                    responMenu.style.position = "absolute";
+                    responMenu.style.top = "0";
+                    responMenu.style.bottom = "0";
+                    responMenu.style.right = "0";
+                    responMenu.style.left = "0";
+                    responMenu.style.zIndex = "100";
+
+                    responMenuList.style.marginTop = "1rem";
+                    // Show the menu with transition
+                    setTimeout(function () {
+                        responMenu.style.opacity = "1";
+                    }, 10); // Delay for transition
+                } else {
+                    // Hide the menu with transition
+                    responMenu.style.opacity = "0";
+                    setTimeout(function () {
+                        responMenu.style.display = "none";
+                    }, 250); // Transition duration
+                }
+                isClicked = !isClicked;
+            });
+        </script>
     </body>
 </html>
--- curtural-events.php
+++ curtural-events.php
@@ -29,6 +29,8 @@ $result = $stmt->fetchAll();
         
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="css/header.css">
+        <link rel="stylesheet" href="css/responsive_header.css">
+        <link rel="stylesheet" href="css/responsive_menu.css">
         
 
         <link rel="stylesheet" media="all" href="barcelona/css_rFRysy6MbVSEpojtuKXhCIczox98m6-B1zLbaxABV_E.css">
@@ -55,77 +57,72 @@ $result = $stmt->fetchAll();
     <body>
 
     <header class="header">
-				<ul type="none">
-					<input type="checkbox" id="menu-check">
-					<li class="logo"><a href="main.html">www.corfu.gr</a></li>
-					<li class="language_container">
-						<div>
-							<i class="fa-solid fa-earth-europe"></i>
-							<select id="languageSelector">
-								<option value="en">Ελληνικά</option>
-								<option value="fr">English</option>
-								<option value="es">Spanish</option>
-							</select>
-						</div>
-					</li>
-					
-					<li class="dummy-list-item"> </li>
-					<li class="menu-btn">
-						<label for="menu-check">
-							<span></span>
-							<span></span>
-							<span></span>
-						</label>
-					</li>
+                <ul type="none">
+                    <input type="checkbox" id="menu-check">
+                    <li class="logo"><a href="main.html">www.corfu.gr</a></li>
+                    <li class="language_container">
+                        <div>
+                            <i class="fa-solid fa-earth-europe"></i>
+                            <select id="languageSelector">
+                                <option value="en">Ελληνικά</option>
+                                <option value="fr">English</option>
+                                <option value="es">Spanish</option>
+                            </select>
+                        </div>
+                    </li>
+                    
+                    <li class="dummy-list-item"> </li>
+                    <li class="menu-btn">
+                        <label for="menu-check" id="menu-btn">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </label>
+                    </li>
 
-					
-					<div id="menu-ciutat-cfu-v1" class="desktop amagat" style="margin-bottom: 4rem;">
-						<div id="menu-ciutat-wrapper">
-							<ul data-region="menu" class="clearfix level-0">
-								<!-- AGGELIES -->
-								<li class="menu-1 expanded dropdown">
-									<a href="adverts.php" class="dropbtn">ΑΓΓΕΛΙΕΣ</a>
-									<div class="dropdown-content">
-										<p>Ανεβάστε τις δικές σας αγγελίες και μοιραστείτε τις ευκαιρίες, τις υπηρεσίες και τα προϊόντα σας με την κοινότητα!</p>
-									</div>
-								</li>
-								<!-- EKDHLWSEIS -->
-								<li class="menu-2 expanded dropdown">
-									<a href="curtural-events.php">ΕΚΔΗΛΩΣΕΙΣ</a>
-									<div class="dropdown-content">
-										<p>Ανακαλύψτε τις πιο hot εκδηλώσεις του νησιού - Από μουσικά events μέχρι πολιτιστικά φεστιβάλ! Ενημερωθείτε και συμμετέχετε στον παλμό της τοπικής ζωής.</p>
-									</div>
-								</li>
-								<li class="menu-3 expanded dropdown">
-									<a href="/en/what-to-do-in-cfu">What to do in cfu</a>
-									<div class="dropdown-content">
-										<p>Ανακαλύψτε τις καλύτερες εμπειρίες και δραστηριότητες στο νησί - Συνδεθείτε με την τοπική κουλτούρα και ανακαλύψτε μοναδικά μέρη για να εξερευνήσετε!</p>
-									</div>
-								</li>
-								<li class="menu-4 expanded dropdown">
-									<a href="/en/discovercfu">Discover cfu</a>
-									<div class="dropdown-content">
-										<p>Ανακαλύψτε τα μυστικά και τις ομορφιές του νησιού - Μια πύλη για να εξερευνήσετε τα πάντα γύρω από την Κέρκυρα!</p>
-									</div>
-								</li>
-								<li class="menu-5 expanded dropdown">
-									<a href="/en/getinvolved">Get involved</a>
-									<div class="dropdown-content">
-										<p>Ανακαλύψτε τα μυστικά και τις ομορφιές του νησιού - Μια πύλη για να εξερευνήσετε τα πάντα γύρω από την Κέρκυρα!</p>
-									</div>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</ul>
-			</header>
+                    <div class="header-menu-container" id="header-menu-container">
+                        <ul id="header-menu-container-list">
+                            <!-- AGGELIES -->
+                            <li class="menu-1 expanded dropdown">
+                                <a href="adverts.php" class="dropbtn">ΑΓΓΕΛΙΕΣ</a>
+                                <div class="dropdown-content">
+                                    <p>Ανεβάστε τις δικές σας αγγελίες και μοιραστείτε τις ευκαιρίες, τις υπηρεσίες και τα προϊόντα σας με την κοινότητα!</p>
+                                </div>
+                            </li>
+                            <!-- EKDHLWSEIS -->
+                            <li class="menu-2 expanded dropdown">
+                                <a href="curtural-events.php">ΕΚΔΗΛΩΣΕΙΣ</a>
+                                <div class="dropdown-content">
+                                    <p>Ανακαλύψτε τις πιο hot εκδηλώσεις του νησιού - Από μουσικά events μέχρι πολιτιστικά φεστιβάλ! Ενημερωθείτε και συμμετέχετε στον παλμό της τοπικής ζωής.</p>
+                                </div>
+                            </li>
+                            <li class="menu-3 expanded dropdown">
+                                <a href="/en/what-to-do-in-cfu">What to do in cfu</a>
+                                <div class="dropdown-content">
+                                    <p>Ανακαλύψτε τις καλύτερες εμπειρίες και δραστηριότητες στο νησί - Συνδεθείτε με την τοπική κουλτούρα και ανακαλύψτε μοναδικά μέρη για να εξερευνήσετε!</p>
+                                </div>
+                            </li>
+                            <li class="menu-4 expanded dropdown">
+                                <a href="/en/discovercfu">Discover cfu</a>
+                                <div class="dropdown-content">
+                                    <p>Ανακαλύψτε τα μυστικά και τις ομορφιές του νησιού - Μια πύλη για να εξερευνήσετε τα πάντα γύρω από την Κέρκυρα!</p>
+                                </div>
+                            </li>
+                            <li class="menu-5 expanded dropdown">
+                                <a href="/en/getinvolved">Get involved</a>
+                                <div class="dropdown-content">
+                                    <p>Ανακαλύψτε τα μυστικά και τις ομορφιές του νησιού - Μια πύλη για να εξερευνήσετε τα πάντα γύρω από την Κέρκυρα!</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </ul>
+            </header>
 
-        <div id="menu-ciutat-cfu-v1" class="desktop amagat" style="margin-bottom: 4rem;">
-            <div id="menu-ciutat-wrapper">
-                <ul data-region="menu" class="clearfix level-0">
+            <div class="menu-container" id="largeMenu" style="margin-bottom: 4rem;">
+                <ul>
                     <!-- AGGELIES -->
                     <li class="menu-1 expanded dropdown">
-                        <!-- <div class="icona"><img src="https://www.barcelona.cat/assetsdi/lameva/menu/img/fletxa_1.png"></div> -->
                         <a href="adverts.php" class="dropbtn">ΑΓΓΕΛΙΕΣ</a>
                         <div class="dropdown-content">
                             <p>Ανεβάστε τις δικές σας αγγελίες και μοιραστείτε τις ευκαιρίες, τις υπηρεσίες και τα προϊόντα σας με την κοινότητα!</p>
@@ -158,7 +155,6 @@ $result = $stmt->fetchAll();
                     </li>
                 </ul>
             </div>
-        </div>
 
         <main id="main-content" role="main">
             <section class="region region-content">
@@ -208,5 +204,40 @@ $result = $stmt->fetchAll();
         <?php $conn = null ?>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script>
+            const menuBtn = document.getElementById("menu-btn");
+            const responMenu = document.getElementById("header-menu-container");
+            const responMenuList = document.getElementById("header-menu-container-list");
+            var isClicked = false;
+
+            
+            menuBtn.addEventListener("click", function () {
+                if (!isClicked) {
+                    //Responsive Menu
+                    responMenu.style.backgroundColor = "whitesmoke";
+                    responMenu.style.display = "flex";
+                    responMenu.style.transition = "opacity 250ms ease-in-out";
+                    responMenu.style.position = "absolute";
+                    responMenu.style.top = "0";
+                    responMenu.style.bottom = "0";
+                    responMenu.style.right = "0";
+                    responMenu.style.left = "0";
+                    responMenu.style.zIndex = "100";
+
+                    responMenuList.style.marginTop = "1rem";
+                    // Show the menu with transition
+                    setTimeout(function () {
+                        responMenu.style.opacity = "1";
+                    }, 10); // Delay for transition
+                } else {
+                    // Hide the menu with transition
+                    responMenu.style.opacity = "0";
+                    setTimeout(function () {
+                        responMenu.style.display = "none";
+                    }, 250); // Transition duration
+                }
+                isClicked = !isClicked;
+            });
+        </script>
     </body>
 </html>
